0d2472df0ca4e6fce98cdc32ec98b38bb29696c3
Chuankai Zhang <chuankaz@andrew.cmu.edu>
2018-02-14 20:36:35 -0500

    first edit

--- Assignments/Byte 2 - Explore/main.py
+++ Assignments/Byte 2 - Explore/main.py
@@ -24,18 +24,18 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 # This API key is provided by google as described in the tutorial
-API_KEY = 'XXxxXxXXXXxxNXXxXXXxxxNNXXxxxxxxxXXXxXX'
+API_KEY = 'AIzaSyBWD2VkD7Rl5eJkzEP9snndRfOTZTaqNrg'
 
 # This uses discovery to create an object that can talk to the 
 # fusion tables API using the developer key
 service = build('fusiontables', 'v1', developerKey=API_KEY)
 
 # This is the table id for the fusion table
-TABLE_ID = 'NxxxNXxXxxNxXXXXNXxXXXxXxxxNxXxNxXxxXxxX'
+TABLE_ID = '1jDNgu9rG3oHDZxOaroIwzK-XFA30Stqt0g9CZQc_'
 
 # This is the default columns for the query
 query_cols = []
-query_values = ['Forlan'] #Change to be the value(s) you're querying in the column you've specified
+query_values = [] #Change to be the value(s) you're querying in the column you've specified
 
 # Import the Flask Framework
 from flask import Flask, request
@@ -71,12 +71,12 @@ def make_query(cols, values, limit):
     string_values = string_values[2:len(string_values)]
     
     #Change this query to have your corresponding column (in our soccer example, the column for our WHERE is Scorer).
-    query = "SELECT " + string_cols + " FROM " + TABLE_ID + " WHERE Scorer = '" + string_values + "'"
+    query = "SELECT " + string_cols + " FROM " + TABLE_ID 
 
     query = query + " LIMIT " + str(limit)
 
     logging.info(query)
-    # query = "SELECT * FROM " + TABLE_ID + " WHERE  Scorer = 'Forlan' LIMIT 5"
+    #query = "SELECT * FROM " + TABLE_ID + " WHERE  Region = 'East' LIMIT 5"
 
     return query
     
